fb9a2699bd48adae7caf5619943062e08774ec0e
amadeusmazzini <83679716+amadeusmazzini@users.noreply.github.com>
2021-08-06 19:24:14 -0300

    Add files via upload

---
+++ CodigoAprox_Simples.jl
@@ -0,0 +1,27 @@
+import Pkg; Pkg.add("QuadGK")
+using LinearAlgebra, QuadGK
+using Plots
+gr()
+
+grau = 2            #Digite o grau do polinômio
+a,b = 0 , 1         #Digite o intervalo [a,b] 
+f(x) = 4x^3         #Digite a f(x) que deseja
+
+
+
+M = zeros(grau+1,grau+1)
+B = zeros(grau+1)
+for i = 1:grau+1  
+    for j =i:grau+1
+        h(x) = x^(i-1)*x^(j-1)
+        M[i,j] = quadgk(h, a, b)[1]
+        M[j,i] = M[i,j]
+    end
+    g(x) = f(x)*x^(i-1)
+    B[i] = quadgk(g, a, b)[1]
+end
+α = M\B             #Resolve M α = B
+P(x) = sum(α[k+1]*x^(k) for k = 0:grau) #Polinômio aproximador
+m = range(0,1, length=100)
+plot(m,f.(m))       #Plota a f
+plot!(m,P.(m))      #Plota a aproximação
